6b8f95c7b42a672ef1f4a1632a1e220fc39fb528
ShibleNK <nomansible69@gmail.com>
2026-02-13 17:03:54 +0000

    article page errors resolve

--- pages/clientSite/HomePage.ts
+++ pages/clientSite/HomePage.ts
@@ -1,4 +1,4 @@
-import {Page, Locator, expect} from '@playwright/test';
+import { Page, Locator, expect } from "@playwright/test";
 
 /**
  * This is the page object for the Home Page.
@@ -7,42 +7,40 @@ import {Page, Locator, expect} from '@playwright/test';
  * @typedef {HomePage}
  */
 
-export class HomePage{
-    constructor(private page: Page){}
+export class HomePage {
+  constructor(private page: Page) {}
 
-    get homeBanner(): Locator{
-        return this.page.getByRole('heading', {name: 'conduit'})
-    }
-    get yourFeedBtn(): Locator{
-        return this.page.getByText('Your Feed')
-    }
-    get globalFeedBtn(): Locator{
-        return this.page.getByRole('link',{
-            name: 'www.bondaracademy.com'
-        })
-    }
-    get noArticlesMessage(): Locator{
-        return this.page.getByText('No articles are here... yet.')
-    }
+  get homeBanner(): Locator {
+    return this.page.getByRole("heading", { name: "conduit" });
+  }
+  get yourFeedBtn(): Locator {
+    return this.page.getByText("Your Feed");
+  }
+  get globalFeedBtn(): Locator {
+    return this.page.getByRole("link", {
+      name: "www.bondaracademy.com",
+    });
+  }
+  get noArticlesMessage(): Locator {
+    return this.page.getByText("No articles are here... yet.");
+  }
 
-    /**
-     * Navigates to the home page as Guest.
-     * @returns {Promise<void>} Resolves when the navigation is complete.
-     */
-    async navigateToHomePageGuest(): Promise<void>{
-        await this.page.goto(process.env.URL as string)
-        await expect(this.homeBanner).toBeVisible()
-    }
+  /**
+   * Navigates to the home page as Guest.
+   * @returns {Promise<void>} Resolves when the navigation is complete.
+   */
+  async navigateToHomePageGuest(): Promise<void> {
+    await this.page.goto(process.env.URL as string);
+    await expect(this.homeBanner).toBeVisible();
+  }
 
-
-    /**
-     * Navigates to the home page as User.
-     * @returns {Promise<void>} Resolves when the navigation is complete.
-     */
-    async navigateToHomePageUser(): Promise<void>{
-        await this.page.goto(process.env.URL as string)
-        await expect(this.yourFeedBtn).toBeVisible()
-        await expect(this.globalFeedBtn).toBeVisible()
-    }
-
+  /**
+   * Navigates to the home page as User.
+   * @returns {Promise<void>} Resolves when the navigation is complete.
+   */
+  async navigateToHomePageUser(): Promise<void> {
+    await this.page.goto(process.env.URL as string);
+    await expect(this.yourFeedBtn).toBeVisible();
+    await expect(this.globalFeedBtn).toBeVisible();
+  }
 }
